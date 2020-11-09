372604e1c609fbbecff9714d58bdd606425c4b60
jumpman <joseph.bollman@gmail.com>
2020-11-08 20:37:27 -0700

    Created a seperate function, new constant to hold 26

--- rotational-cipher/RotationalCipher.cs
+++ rotational-cipher/RotationalCipher.cs
@@ -1,23 +1,29 @@
 using System;
 public static class RotationalCipher
 {
-        private const int UPPER_CASE = 65;
-        private const int LOWER_CASE = 97;
+    private const int UPPER_CASE = 65;
+    private const int LOWER_CASE = 97;
+    private const int TOTAL_LETTERS_IN_ALPHABET = 26;
     public static string Rotate(string text, int shiftKey)
     {
-       var returnString = "";
+        var returnString = "";
        foreach (char c in text)
        {
-           if (char.IsLetter(c))
-           {
-               var lowerOrUpper = char.IsUpper(c) ? UPPER_CASE : LOWER_CASE;
-               returnString += (char)(lowerOrUpper + (c + shiftKey - lowerOrUpper) % 26);
-           }
-           else
-           {
-               returnString += c;
-           }
-        }
+           returnString += Validate(c, shiftKey);
+       }
        return returnString;
+    }
+
+    private static char Validate(char c, int shiftKey)
+    {
+        if (char.IsLetter(c))
+        {
+            var lowerOrUpper = char.IsUpper(c) ? UPPER_CASE : LOWER_CASE;
+           return (char)(lowerOrUpper + (c + shiftKey - lowerOrUpper) % TOTAL_LETTERS_IN_ALPHABET);
+        }
+        else
+        {
+            return c;
+        }
     }
 }

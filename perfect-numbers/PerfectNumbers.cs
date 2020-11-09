98ecdead38d0140a625e2e59c86acf02822cf037
jumpman <joseph.bollman@gmail.com>
2020-11-08 20:59:21 -0700

    Added a new if statement. Made the algorithm slightly better than O(n) because it will not check every number beneath argument because no sub number will go into the larger number an even amount of times if the sub number is .50% + 1 of the original number.

--- perfect-numbers/PerfectNumbers.cs
+++ perfect-numbers/PerfectNumbers.cs
@@ -8,35 +8,33 @@ public enum Classification
 
 public static class PerfectNumbers
 {
-
     public static Classification Classify(int number)
     {
         var sumNumber = 0;
 
-            if (number <= 0)
-            {
-                throw new ArgumentOutOfRangeException();
-            }
+        if (number <= 0)
+        {
+            throw new ArgumentOutOfRangeException();
+        }
 
-            for ( var i = 0; i < number; i++)
-            {
-                if (i ==0)
-                    continue;
-                if (number % i == 0)
-                    sumNumber += i;
-            }
-
-            if (sumNumber > number)
-            {
+        for (var i = 1; i < number; i++)
+        {
+            if (number % i == 0)
+                sumNumber += i;
+            if ((number / 2) <= i)
+                break;
+        }
+        if (sumNumber > number)
+        {
             return Classification.Abundant;
-            }
-            else if(sumNumber < number)
-            {
-                return Classification.Deficient;
-            }
-            else
-            {
-                return Classification.Perfect;
-            }
+        }
+        else if(sumNumber < number)
+        {
+            return Classification.Deficient;
+        }
+        else
+        {
+            return Classification.Perfect;
+        }
     }
 }

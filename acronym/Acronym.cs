96354e1b502505a569f4a2ccff6b30f45d8cd61e
jumpman <joseph.bollman@gmail.com>
2020-11-09 12:43:19 -0700

    Two ways to solve it. Must get better practice with LINQ. every foreach should be able to be replaced with a LINQ statement.

---
+++ acronym/Acronym.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+public static class Acronym
+{
+    //LINQ way
+    public static string Abbreviate(string phrase) => string.Join("", phrase.Split(new char[] {'-','_',' '}, StringSplitOptions.RemoveEmptyEntries).Select(w => w.ToString().ToUpper().First()));
+    // more traditional way.
+    /*{
+        var returnString = "";
+        var words = phrase.Split(new char[] {'-','_',' '}, StringSplitOptions.RemoveEmptyEntries);
+        foreach (var w in words)
+        {
+            returnString += w[0].ToString().ToUpper();
+        }
+        return returnString;
+    }
+    */
+}

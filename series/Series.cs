b472dbe5ba2bb4ff52bc88a1e7230e83d987ff3e
jumpman <joseph.bollman@gmail.com>
2020-11-16 11:46:37 -0700

    removed commented out code

--- series/Series.cs
+++ series/Series.cs
@@ -17,23 +17,3 @@ public static class Series
         return mySlices.ToArray();
     }
 }
-        
-        /*
-        var query =
-            numbers.Select((fruit, index) =>
-                new { sliceLength, str = num.Substring(0, sliceLength) });
-        //string[] mySlices = new string[];
-        
-        //numbers.Select().Take(sli
-        /*
-        mySlices = numbers.ToArray().GroupBy(sliceLength);
-        Enumerable<string> query =
-            numbers.Take(sliceLength);
-            //numbers.TakeWhile(fruit => String.Compare("orange", fruit, true) != 0)
-                //.Append(mySlices);
-               mySlices.Add(query);
-               
-                //return mySlices;
-                //var slices =  (IEnumerable)numbers.Take(sliceLength);
-                //return (string[])slices;
-        */
